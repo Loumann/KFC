88031ead11815de6553a5dc10c2f32b1557d6bf3
bleyk <bleyk@DESKTOP-8G2B3FF>
2022-04-17 00:29:07 +0300

    Добавьте файлы проекта.

---
+++ KFC/LogonList.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace KFC
+{
+    /// <summary>
+    /// Логика взаимодействия для LogonList.xaml
+    /// </summary>
+    public partial class LogonList : Page
+    {
+        ContentMenu _contentMenu = new ContentMenu();
+
+        public LogonList()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (LoginTextBox.Text.Length == 0)
+            {
+                MessageBox.Show("xuy");
+            
+            }
+            else if (LoginTextBox.Text.Length == 1)
+            {
+                if (PasstextBox.Text.Length == 0)
+                {
+                    MessageBox.Show("zalupa");
+                }
+                else if (PasstextBox.Text.Length == 1 )
+                {
+
+                }
+            }
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            NavigationService nav = NavigationService.GetNavigationService(this);
+            nav.Navigate(new ContentMenu());
+
+
+        }
+    }
+}
---
+++ KFC/MainWindow.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System.Windows;
+
+namespace KFC
+{
+    public partial class MainWindow : Window
+    {
+        LogonList logonList = new LogonList();
+        public MainWindow()
+        {
+            InitializeComponent();
+           MainFrame.Content = logonList;
+
+        }
+
+        private void MainFrame_Navigated(object sender, System.Windows.Navigation.NavigationEventArgs e)
+        {
+        }
+    }
+}
